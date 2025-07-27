5f84149ecd0ccdb819f86f14bd86730c8c9fdf72
Mawrak <jenialuk@rambler.ru>
2025-07-27 05:11:29 +0300

    Updated light shaders on r1 and r2
    lplanes shaders are used as a base
    r1 is 100% an improvement
    in r2 I had to do this to avoid issues with the inconsistent sun mask. Hopefully will find better solution

--- gamedata/shaders/r1/selflight.s
+++ gamedata/shaders/r1/selflight.s
@@ -1,16 +1,9 @@
-function normal	(shader, t_base, t_second, t_detail)
-shader:begin	("simple","simple")
-: fog	(true)
-shader:sampler	("s_base") :texture	(t_base)
-end
-function l_spot		(shader, t_base, t_second, t_detail)
-r1_lspot (shader, t_base, "simple_spot")
-end
-function l_point	(shader, t_base, t_second, t_detail)
-r1_lpoint 	(shader, t_base, "simple_point")
-end
-function l_special	(shader, t_base, t_second, t_detail)
-shader:begin	("simple","simple")
+function normal (shader, t_base, t_second, t_detail)
+shader:begin ("model_def_lplanes","base_lplanes_lampa")
 : fog	(false)
-shader:sampler	("s_base") :texture	(t_base)
+: zb	(true,false)
+: blend (true,blend.srcalpha,blend.one)
+: aref (true,0)
+: sorting (2, false)
+shader:sampler ("s_base") :texture (t_base)
 end
--- gamedata/shaders/r2/models_selflightl.s
+++ gamedata/shaders/r2/models_selflightl.s
@@ -1,12 +1,10 @@
 function normal	(shader, t_base, t_second, t_detail)
-shader:begin	("deffer_model_flat","deffer_base_flat")
-: fog	(false)
-: emissive (true)
-shader:sampler	("s_base") :texture	(t_base)
-end
-function l_special	(shader, t_base, t_second, t_detail)
-shader:begin	("shadow_direct_model",	"accum_emissivel")
+shader:begin 	("model_def_lplanes","base_lplanes")
+: fog 	(false)
 : zb 	(true,false)
-: fog	(false)
-: emissive (true)
+: blend 	(true,blend.srcalpha,blend.one)
+: aref 	(true,0)
+: sorting	(2, true)
+shader:sampler	("s_base") :texture (t_base)
 end
+
